bce5630a2b5190969f3e3de550e8159fb1b4fa45
Joe Ferner <joe@fernsroth.com>
2021-10-14 01:07:12 +0000

    call out on button press

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ socat -d -d pty,raw,echo=0 pty,raw,echo=0
 2021/09/19 02:08:25 socat[145655] N starting data transfer loop with FDs [5,5] and [7,7]
 
 cargo run -- --file test.json --port /dev/pts/6
-echo '!s123' > /dev/pts/7
+printf '!s100\n!s200\n!s300\n' > /dev/pts/7
 ```
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ extern crate rocket;
 
 use clap::App;
 use clap::Arg;
+use log::{error, info};
 use raspberry_pi_ir_hat::Config;
-use raspberry_pi_ir_hat::{Hat, HatError};
+use raspberry_pi_ir_hat::{Hat, HatError, HatMessage};
 use rocket::http::ContentType;
 use rocket::response::Responder;
 use rocket::State;
+use simple_logger::SimpleLogger;
 use std::sync::Mutex;
 
 struct MyState {
@@ -79,7 +81,25 @@ fn transmit(state: &State<MyState>, remote_name: &str, button_name: &str) -> MyR
     };
 }
 
+fn send_button_press(url_template: &String, remote_name: &String, button_name: &String) {
+    let url = url_template
+        .replace("{remoteName}", &remote_name)
+        .replace("{buttonName}", &button_name);
+    info!(
+        "button press: {} {}, calling {}",
+        remote_name, button_name, url
+    );
+    match ureq::post(&url).call() {
+        Result::Ok(_response) => {}
+        Result::Err(err) => {
+            error!("failed to call url: {}: {}", url, err);
+        }
+    };
+}
+
 fn init() -> std::result::Result<Hat, String> {
+    SimpleLogger::new().init().map_err(|err| format!("{}", err));
+
     let args = App::new("UnisonHT - Raspberry Pi IrHat")
         .version("1.0.0")
         .author("Joe Ferner <joe@fernsroth.com>")
@@ -91,6 +111,12 @@ fn init() -> std::result::Result<Hat, String> {
                 .value_name("FILE")
                 .help("File to load IR signals to")
                 .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("button_press_target_url")
+                .long("button-press-target-url")
+                .help("URL to POST send button presses to (http://localhost/button-press/{remoteName}/{buttonName})")
                 .takes_value(true),
         )
         .arg(
@@ -115,6 +141,9 @@ fn init() -> std::result::Result<Hat, String> {
     let config = Config::read(config_filename, false)
         .map_err(|err| format!("failed to read config {}", err))?;
     let port = args.value_of("port").unwrap();
+    let button_press_target_url: Option<String> = args
+        .value_of("button_press_target_url")
+        .map(|s| s.to_owned());
     let tolerance = args
         .value_of("tolerance")
         .unwrap()
@@ -130,8 +159,27 @@ fn init() -> std::result::Result<Hat, String> {
         config,
         port,
         tolerance,
-        Box::new(|message| {
-            println!("{:#?}", message);
+        Box::new(move |message| {
+            match message {
+                HatMessage::ButtonPress(button_press) => match &button_press_target_url {
+                    Option::None => {
+                        info!(
+                            "button press: {} {}",
+                            button_press.remote_name, button_press.button_name
+                        );
+                    }
+                    Option::Some(url) => {
+                        send_button_press(
+                            url,
+                            &button_press.remote_name,
+                            &button_press.button_name,
+                        );
+                    }
+                },
+                HatMessage::Error(err) => {
+                    error!("{:#?}", err);
+                }
+            };
         }),
     );
     hat.open()
@@ -144,7 +192,7 @@ fn init() -> std::result::Result<Hat, String> {
 fn rocket() -> _ {
     match init() {
         Result::Err(err) => {
-            println!("{}", err);
+            error!("{}", err);
             std::process::exit(1);
         }
         Result::Ok(hat) => rocket::build()
